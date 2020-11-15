2f79c4946cd6eb6e40172b8e080ab01edf92997f
Tomoyuki Hata <hato6502@gmail.com>
2020-11-15 21:40:27 +0900

    add id property to OutputBlockData

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,36 @@
+import type { OutputBlockData, OutputData } from "@editorjs/editorjs";
 import type { FunctionComponent } from "react";
 
-const ALTEditor: FunctionComponent = () => <div>ALTEditor</div>;
+interface ALTEditorOutputData extends OutputData {
+  blocks: ALTEditorOutputBlockData[];
+}
+
+interface ALTEditorOutputBlockData extends OutputBlockData {
+  id: string;
+}
+
+const outputData: ALTEditorOutputData = {
+  blocks: [
+    {
+      id: "0000",
+      type: "text",
+      data: {
+        text: "<p>TEST</p>",
+      },
+    },
+  ],
+};
+
+const ALTEditor: FunctionComponent = () => (
+  <div>
+    {outputData.blocks.map(({ id, type, data }) => (
+      <div key={id}>
+        <div>type: {type}</div>
+        <div>data: {JSON.stringify(data)}</div>
+      </div>
+    ))}
+  </div>
+);
 
 export { ALTEditor };
+export type { ALTEditorOutputData, ALTEditorOutputBlockData };
